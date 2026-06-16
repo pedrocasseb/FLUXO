da762591150b01b5850dc01b4f0deeada75af027
Pedro Casseb <casseb.phcc@gmail.com>
2026-06-16 10:44:16 -0300

    refactor(security): move JWT secret fallback exclusively to test configuration

--- server/src/test/java/com/pedrocasseb/fluxo/FluxoApplicationTests.java
+++ server/src/test/java/com/pedrocasseb/fluxo/FluxoApplicationTests.java
@@ -3,7 +3,7 @@ package com.pedrocasseb.fluxo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "jwt.secret=d7c189b8822556b26b3bfdf8d27038e219fbcfb93eaee89b9a67bc647db2de74")
 class FluxoApplicationTests {
 
 	@Test
